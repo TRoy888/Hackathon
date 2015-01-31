836bc1d99af149503aef8fa1469029db415af660
sept1412 <sept1412@gmail.com>
2015-01-31 06:48:10 -0800

    Add css to sing in and log in pages

--- web-services/app/views/users/create.blade.php
+++ web-services/app/views/users/create.blade.php
@@ -1,25 +1,36 @@
-{{Form::open(['route'=>'user.store'])}}
-  <div>
-    {{Form::label('email', 'Email: ')}}
-    {{Form::text('email')}}
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <h2 class="form-signin-heading">Sign in</h2>
+    {{ Form::open(['route'=>'user.store'], array('class'=>'form-signin')) }}
+      <div>
+        {{Form::text('email', null, array('class'=>'form-control', 'placeholder'=>'E-mail address')) }}
+      </div>
+      <div>
+        {{ Form::text('first_name', null, array('class'=>'form-control', 'placeholder'=>'First name')) }}
+      </div>
+      <div>
+        {{ Form::text('last_name', null, array('class'=>'form-control', 'placeholder'=>'Last name')) }}
+      </div>
+      <div>
+        {{ Form::password('password', array('class'=>'form-control', 'placeholder'=>'Password')) }}
+      </div>
+      <div>
+        {{ Form::text('detail', null, array('class'=>'form-control', 'placeholder'=>'Detail')) }}
+      </div>
+      <div>
+        {{ Form::submit('Sign in', array('class'=>'btn btn-lg btn-info btn-block')) }}
+      </div>
+    {{Form::close()}}
   </div>
-  <div>
-    {{Form::label('first_name', 'First Name: ')}}
-    {{Form::text('first_name')}}
-  </div>
-  <div>
-    {{Form::label('last_name', 'Last Name: ')}}
-    {{Form::text('last_name')}}
-  </div>
-  <div>
-    {{Form::label('password', 'Password: ')}}
-    {{Form::text('password')}}
-  </div>
-  <div>
-    {{Form::label('detail', 'Detail: ')}}
-    {{Form::text('detail')}}
-  </div>
-  <div>
-    {{Form::submit('Sign in')}}
-  </div>
-{{Form::close()}}
+</body>
+</html>
