43e36bb71857a3f63fc58ad129d7fddc983a05ab
Title <ks8pd@virginia.edu>
2015-01-31 01:44:46 -0800

    added swipe and refresh list view

--- ActivityHub/app/src/main/java/hackathon/activityhub/MainActivity.java
+++ ActivityHub/app/src/main/java/hackathon/activityhub/MainActivity.java
@@ -2,16 +2,53 @@ package hackathon.activityhub;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
 
+    private SwipeRefreshLayout refreshview;
+    private Handler handler = new Handler();
+    private ArrayList<String> list = new ArrayList<String>();
+    private ListView lv;
+    private ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView)findViewById(R.id.listView);
+        list.add("test1");
+        list.add("test2");
+        arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,list
+        );
+        lv.setAdapter(arrayAdapter);
+        refreshview = (SwipeRefreshLayout)findViewById(R.id.swipe);
+        //refreshview.setColorSchemeResources(R.color.blue_bright, R.color.green_light,
+        //    R.color.orange_light, R.color.red_light);
+        refreshview.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // get the new data from you data source
+                // TODO : request data here
+                // our swipeRefreshLayout needs to be notified when the data is returned in order for it to stop the animation
+                handler.postDelayed(new Runnable(){
+                    public void run(){
+                        list.add("testAdd");
+                        refreshview.setRefreshing(false);
+                    }
+                },500);
+                lv.setAdapter(arrayAdapter);
+            }
+        });
     }
 
 
