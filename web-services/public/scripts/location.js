8aa0fb739e9517305152a078144d2701b1d715bd
TRoy888 <mr.punyawee@gmail.com>
2015-01-31 06:26:31 -0800

    get location function

---
+++ web-services/public/scripts/location.js
@@ -0,0 +1,13 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("location is not supported");
+    }
+}
+function showPosition(position) {
+  var p = {
+      'lon':position.coords.longitude
+      , 'lat':position.coords.latitude
+  };
+}
